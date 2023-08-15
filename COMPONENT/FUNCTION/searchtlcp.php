8bcbb5ef5d18a569b8149b18d4957857f3852467
Izzrieq <izzrieqilhan@gmail.com>
2023-08-15 10:09:58 +0800

    staging

--- COMPONENT/FUNCTION/searchtlcp.php
+++ COMPONENT/FUNCTION/searchtlcp.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+
     <!-- Include the Tailwind CSS script -->
     <script src="https://cdn.tailwindcss.com"></script>
-    
+
     <!-- Other CSS links -->
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -23,8 +23,8 @@
     <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
     <!-- MDB -->
-<?php
-include("..//DB/config.php");
+<?php 
+ include("../DB/config.php");
 
     $searchQuery = $_POST['lcid'];
     $start = 0;
