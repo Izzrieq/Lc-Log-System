91017dcca463c2da0c466bb794fb9a6c9e98d657
Nasy08 <[anassuhaimi08@gmail.com]>
2023-08-25 17:40:47 +0800

    Adjust layout

--- COMPONENT/footer.php
+++ COMPONENT/footer.php
@@ -1,8 +1,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-<footer class="fixed-bottom bg-gray-700 text-white p-2 align-middle text-center">
-            <p class="font-bold font-size-20px">© 2023 Niko & Nasy</p>
+<footer class="fixed-bottom bg-gray-700 text-white align-middle text-center">
+            <p class="font-bold font-size-20px pt-2">© 2023 Niko & Nasy</p>
     </footer>
 </body>
 </html>
